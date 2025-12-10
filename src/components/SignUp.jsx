867eb139cddf1eacb0fa33a8ec2ff526d8e41192
farsi03 <farseenaf223@gmail.com>
2025-12-10 12:18:24 +0530

    Add files via upload

---
+++ src/components/SignUp.jsx
@@ -0,0 +1,13 @@
+import { TextField } from '@mui/material'
+import React from 'react'
+
+const SignUp = () => {
+  return (
+    <div>
+        <h3>Signup</h3>
+        <TextField label="Name" variant="outlined" fullWidth margin="normal" />
+    </div>
+  )
+}
+
+export default SignUp
